85178386d666d925cef537cfb90bbd34939a4645
TakahiroTada <takass0119@yahoo.co.jp>
2024-02-10 19:06:00 +0900

    initialze

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+{/*import RecipeTitle from './RecipeTitle';*/}
+{/*import './index.css';*/}
+
+function App() {
+
+    return (
+        <article>
+            <h1>利用履歴</h1>
+        </article>
+    )
+}
+
+export default App;
